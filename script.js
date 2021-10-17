1ad142ca530613a141a74a6a7f82d0981f4d3e50
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-10-17 16:38:27 +0200

    Created to-do items array

---
+++ script.js
@@ -0,0 +1,17 @@
+const toDoItems = [
+  {
+    timestamp: 1,
+    text: "Eat something",
+    completed: true,
+  },
+  {
+    timestamp: 2,
+    text: "Practice coding",
+    completed: false,
+  },
+  {
+    timestamp: 3,
+    text: "Save the world",
+    completed: false,
+  },
+];
